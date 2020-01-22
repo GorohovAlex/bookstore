6b1bc8528c7ce6bc879d1f0dad4009edfcdd470f
GorohovAlex <gorochov.as@gmail.com>
2020-01-22 15:37:10 +0200

    fix rubocop bugs

--- db/migrate/20200121105746_create_join_table.rb
+++ db/migrate/20200121105746_create_join_table.rb
@@ -1,8 +1,8 @@
 class CreateJoinTable < ActiveRecord::Migration[6.0]
   def change
     create_join_table :books, :authors do |t|
-      t.index [:book_id, :authors_id]
-      t.index [:authors_id, :book_id]
+      t.index %i[book_id authors_id]
+      t.index %i[authors_id book_id]
     end
   end
 end
