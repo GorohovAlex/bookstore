e6773bd939e8041d5d03b492378ffad217e5521f
GorohovAlex <gorochov.as@gmail.com>
2020-01-24 16:56:27 +0200

    Fix tests

--- spec/features/pages/home_spec.rb
+++ spec/features/pages/home_spec.rb
@@ -1,4 +1,10 @@
-describe 'Home page' do
+describe 'Home page', type: :feature do
+  before do
+    author = Author.create(first_name: 'FirstName', last_name: 'LastName')
+    category = Category.create(name: 'Web')
+    Book.create(name: 'My Book', authors: [author], description: 'text', category: category)
+  end
+
   it 'Visitor registers successfull' do
     visit root_path
     expect(page).to have_content 'Bookstore'
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,9 +15,27 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+end
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before do
+    DatabaseCleaner.start
+    Rails.application.load_seed
+  end
+
+  config.after do
+    DatabaseCleaner.clean
+  end
 end
