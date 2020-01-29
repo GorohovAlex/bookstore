36048aed43f0e0faca832ab3dc51e57b29436607
GorohovAlex <gorochov.as@gmail.com>
2020-01-29 16:47:39 +0200

    Add SORT_LIST

---
+++ config/initializers/constants.rb
@@ -0,0 +1,6 @@
+SORT_LIST = [
+  { type: :newest_first,  value: 'Newest first' },
+  { type: :popular_first, value: 'Popular first' },
+  { type: :price_asc,     value: 'Price: Low to hight' },
+  { type: :price_desc,    value: 'Price: Hight to low' }
+]
