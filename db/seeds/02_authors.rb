942da674c5d3fb00a1e96006f1879fab30257f21
GorohovAlex <gorochov.as@gmail.com>
2020-01-30 12:42:57 +0200

    Remove symbol  '

--- db/seeds/02_authors.rb
+++ db/seeds/02_authors.rb
@@ -1,3 +1,3 @@
 30.times do
-  Author.create(first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name)
+  Author.create(first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name.gsub(/\W/, ''))
 end
