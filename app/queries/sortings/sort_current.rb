53ba27494ca7feaf85f80e60a2040bd6d2f4856a
GorohovAlex <gorochov.as@gmail.com>
2020-02-01 10:21:03 +0200

    Fix bug with Sorting

--- app/queries/sortings/sort_current.rb
+++ app/queries/sortings/sort_current.rb
@@ -3,6 +3,8 @@ module Sortings
     extend Constants
 
     def self.call(sort_selected, relation: Constants::SORT_LIST)
+      return relation[0][:value] if sort_selected.nil?
+
       sort_item = relation.detect { |sort| sort[:type] == sort_selected.to_sym } || relation[0]
       sort_item[:value]
     end
