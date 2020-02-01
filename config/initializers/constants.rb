bd230e008342a681f4d954a5c8f0802609ec4152
GorohovAlex <gorochov.as@gmail.com>
2020-02-01 10:17:35 +0200

    Add values to SORT_LIST

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,10 +1,10 @@
 module Constants
-
-SORT_LIST = [
-  { type: :newest_first,  value: 'Newest first' },
-  { type: :popular_first, value: 'Popular first' },
-  { type: :price_asc,     value: 'Price: Low to hight' },
-  { type: :price_desc,    value: 'Price: Hight to low' }
-]
-
+  SORT_LIST = [
+    { type: :newest_first,  value: 'Newest first' },
+    { type: :popular_first, value: 'Popular first' },
+    { type: :price_asc,     value: 'Price: Low to hight' },
+    { type: :price_desc,    value: 'Price: Hight to low' },
+    { type: :price_desc,    value: 'Title: A - Z' },
+    { type: :price_desc,    value: 'Title: Z - A' }
+  ].freeze
 end
