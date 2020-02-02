5ad95939472320a168cf971c2da26bd2304af8d1
GorohovAlex <gorochov.as@gmail.com>
2020-02-02 17:04:07 +0200

    Add localization to sorting

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,14 +1,14 @@
 module Constants
-
-  SORT_ITEM_DEFAULT = :title_asc
   SORT_LIST = [
-    { type: :newest_first,  value: 'Newest first' },
-    { type: :popular_first, value: 'Popular first' },
-    { type: :price_asc,     value: 'Price: Low to hight' },
-    { type: :price_desc,    value: 'Price: Hight to low' },
-    { type: :title_asc,    value: 'Title: A - Z' },
-    { type: :title_desc,    value: 'Title: Z - A' }
+    { type: :newest_first,  value: 'sort_list.newest_first',  sort_field: :create_at,   sort_direction: :desc },
+    { type: :popular_first, value: 'sort_list.popular_first', sort_field: :id,          sort_direction: :asc  },
+    { type: :price_asc,     value: 'sort_list.price_asc',     sort_field: :price_cents, sort_direction: :asc  },
+    { type: :price_desc,    value: 'sort_list.price_desc',    sort_field: :price_cents, sort_direction: :desc },
+    { type: :title_asc,     value: 'sort_list.title_asc',     sort_field: :name,        sort_direction: :asc  },
+    { type: :title_desc,    value: 'sort_list.title_desc',    sort_field: :name,        sort_direction: :desc }
   ].freeze
+
+  SORT_ITEM_DEFAULT = SORT_LIST.detect { |sort| sort[:type] == :title_asc }
 
   REGEX_LETTER_ONLY = /\A[a-zA-Z]+\z/.freeze
   REGEX_TITLE = %r{\A[a-zA-Z!#$%&'*+-\/=?^_`{|}~\s]+\z}.freeze
