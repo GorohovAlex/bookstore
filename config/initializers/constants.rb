57e2f1bb58baad3f2e8055c19f3b40ee69bc3147
GorohovAlex <gorochov.as@gmail.com>
2020-02-02 17:08:05 +0200

    Fix bug with 'Newest first'

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,11 +1,11 @@
 module Constants
   SORT_LIST = [
-    { type: :newest_first,  value: 'sort_list.newest_first',  sort_field: :create_at,   sort_direction: :desc },
-    { type: :popular_first, value: 'sort_list.popular_first', sort_field: :id,          sort_direction: :asc  },
-    { type: :price_asc,     value: 'sort_list.price_asc',     sort_field: :price_cents, sort_direction: :asc  },
-    { type: :price_desc,    value: 'sort_list.price_desc',    sort_field: :price_cents, sort_direction: :desc },
-    { type: :title_asc,     value: 'sort_list.title_asc',     sort_field: :name,        sort_direction: :asc  },
-    { type: :title_desc,    value: 'sort_list.title_desc',    sort_field: :name,        sort_direction: :desc }
+    { type: :newest_first,  value: 'sort_list.newest_first',  sort_field: :created_at,   sort_direction: :desc },
+    { type: :popular_first, value: 'sort_list.popular_first', sort_field: :id,           sort_direction: :asc  },
+    { type: :price_asc,     value: 'sort_list.price_asc',     sort_field: :price_cents,  sort_direction: :asc  },
+    { type: :price_desc,    value: 'sort_list.price_desc',    sort_field: :price_cents,  sort_direction: :desc },
+    { type: :title_asc,     value: 'sort_list.title_asc',     sort_field: :name,         sort_direction: :asc  },
+    { type: :title_desc,    value: 'sort_list.title_desc',    sort_field: :name,         sort_direction: :desc }
   ].freeze
 
   SORT_ITEM_DEFAULT = SORT_LIST.detect { |sort| sort[:type] == :title_asc }
