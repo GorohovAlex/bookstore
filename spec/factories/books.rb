9201b0035a3e7e5e9c490e75a062ef41f1dcd46c
GorohovAlex <gorochov.as@gmail.com>
2020-02-03 12:49:24 +0200

    Change Books model and factories

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
     category
     year_of_publication { FFaker::Random.rand(1981..2019) }
 
-    trait :authors do
+    trait :with_authors do
       authors { create_list :author, FFaker::Random.rand(1..2) }
     end
   end
