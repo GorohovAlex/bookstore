0714bc63b4e9c29f8f56c3c98ddfb5b301a933fa
GorohovAlex <gorochov.as@gmail.com>
2020-02-07 22:55:47 +0200

    Realization validate in Sign In

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_filter :configure_permitted_parameters, if: :devise_controller?
   include Pagy::Backend
-
-  protected
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_in) do |u|
-        u.permit :email, :password
-      end
-    end
 end
