9eb41a78250a5c88bd55936ef43e22e7ecfd1dea
GorohovAlex <gorochov.as@gmail.com>
2020-02-07 19:15:53 +0200

    Realization validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
-  # PASSWORD_FORMAT_REGEX = /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}\z/.freeze
+  PASSWORD_FORMAT_REGEX = /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}\z/.freeze
 
   # # validates :email,
   # #           presence: true
-  # # validates :password,
-  # #           presence: true,
-  # #           format: { with: PASSWORD_FORMAT_REGEX, message: "error ha-ha" }
+  validates :password,
+            presence: true,
+            format: { with: PASSWORD_FORMAT_REGEX, message: 'error ha-ha' }
 
   devise :database_authenticatable,
          :registerable,
@@ -26,7 +26,7 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
-      user.encrypted_password = Devise.friendly_token[0, 20]
+      user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name
       user.image = auth.info.image
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   resources :books, only: [:show]
 
-  # devise_scope :user do
-  #   delete '/user/sign_out' => 'devise/sessions#destroy'
-  #   get '/user/log_in' => 'devise/sessions#new'
-  # end
-
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   get 'catalog', to: 'page#catalog'
