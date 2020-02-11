8c12dfce2a502cf7a73ae2db55cc7ba6c7ff42be
GorohovAlex <gorochov.as@gmail.com>
2020-02-11 17:01:31 +0200

    Add eager loading

--- app/queries/books/best_sellers.rb
+++ app/queries/books/best_sellers.rb
@@ -2,7 +2,7 @@ module Books
   class BestSellers
     DEAFULT_QUANTITY = 4
 
-    def self.call(relation: Book.all, quantity: DEAFULT_QUANTITY)
+    def self.call(relation: Book.includes([:authors]), quantity: DEAFULT_QUANTITY)
       relation.order(price_cents: :desc).limit(quantity)
     end
   end
