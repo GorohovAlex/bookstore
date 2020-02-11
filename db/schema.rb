6f6c0273895c638e168244790627634cefb9106b
GorohovAlex <gorochov.as@gmail.com>
2020-02-11 17:06:06 +0200

    Change shema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_04_136741) do
+ActiveRecord::Schema.define(version: 2020_02_11_121548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2020_02_04_136741) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "books_count"
   end
 
   create_table "materials", force: :cascade do |t|
