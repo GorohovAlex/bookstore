ca33f4cd88fb7dca0c78a7704f7eafe49d530183
GorohovAlex <gorochov.as@gmail.com>
2020-02-12 14:20:44 +0200

    Fix some bugs

--- app/queries/sortings/sort_current.rb
+++ app/queries/sortings/sort_current.rb
@@ -1,7 +1,5 @@
 module Sortings
   class SortCurrent
-    extend Constants
-
     def self.call(sort_selected: nil, relation: Constants::SORT_LIST)
       return Constants::SORT_ITEM_DEFAULT if sort_selected.nil?
 
