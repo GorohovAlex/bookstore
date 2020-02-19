b4f3375e8a9c6289fdf805526de49d0bdb8b9a9c
GorohovAlex <gorochov.as@gmail.com>
2020-02-19 10:45:28 +0200

    Config simple_form style

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -46,7 +46,7 @@ SimpleForm.setup do |config|
   # vertical forms
   #
   # vertical default_wrapper
-  config.wrappers :vertical_form, tag: 'div', class: 'form-group', error_class: 'form-group-invalid', valid_class: 'form-group-valid' do |b|
+  config.wrappers :vertical_form, tag: 'div', class: 'form-group', error_class: 'form-group-invalid has-error', valid_class: 'form-group-valid' do |b|
     b.use :html5
     b.use :placeholder
     b.optional :maxlength
@@ -54,9 +54,9 @@ SimpleForm.setup do |config|
     b.optional :pattern
     b.optional :min_max
     b.optional :readonly
-    b.use :label
+    b.use :label, class: 'control-label'
     b.use :input, class: 'form-control', error_class: 'is-invalid', valid_class: 'is-valid'
-    b.use :full_error, wrap_with: { tag: 'div', class: 'invalid-feedback' }
+    b.use :full_error, wrap_with: { tag: 'div', class: 'invalid-feedback text-danger' }
     b.use :hint, wrap_with: { tag: 'small', class: 'form-text text-muted' }
   end
 
