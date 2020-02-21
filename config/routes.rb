bbb0522ebe8f4f588c02da39c974748a73df8d52
GorohovAlex <gorochov.as@gmail.com>
2020-02-21 10:39:14 +0200

    Rename Controller Settings to User

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,16 @@ Rails.application.routes.draw do
   resources :books, only: [:show]
 
   devise_for :users, controllers: {
-    omniauth_callbacks: 'users/omniauth_callbacks'
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/user'
   }
 
   devise_scope :user do
-    get   'users/settings',         to: 'users/settings#update',                  as: 'user_settings'
-    patch 'users/settings',         to: 'users/settings#update_password',         as: 'update_password'
-    patch 'users/billing_address',  to: 'users/settings#update_billing_address',  as: 'users_billing_address_forms'
-    patch 'users/shipping_address', to: 'users/settings#update_shipping_address', as: 'users_shipping_address_forms'
-    patch 'users/update_email',     to: 'users/settings#update_email',            as: 'update_email'
+    get   'user',                  to: 'users/user#index',            as: 'user'
+    patch 'user/billing_address',  to: 'users/user#billing_address',  as: 'users_billing_address_forms'
+    patch 'user/email',            to: 'users/user#email',            as: 'update_user_email'
+    patch 'user/password',         to: 'users/user#password',         as: 'update_user_password'
+    patch 'user/shipping_address', to: 'users/user#shipping_address', as: 'users_shipping_address_forms'
   end
 
   get 'catalog', to: 'page#catalog'
