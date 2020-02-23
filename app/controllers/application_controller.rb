1b5e472cb2c41d6054721abefac61aa574082649
GorohovAlex <gorochov.as@gmail.com>
2020-02-23 12:37:33 +0200

    Add pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
+  include Pundit
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+ 
+  private
+ 
+  def user_not_authorized
+    flash[:alert] = t('.not_authorized')
+    redirect_to(request.referrer || root_path)
+  end
 end
