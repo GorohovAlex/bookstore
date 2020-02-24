6ca1222e53f38598ffef431cd475d8fc6c84b2ff
GorohovAlex <gorochov.as@gmail.com>
2020-02-24 11:40:10 +0200

    Add user policy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
   include Pundit
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
- 
+
   private
- 
+
   def user_not_authorized
     flash[:alert] = t('.not_authorized')
     redirect_to(request.referrer || root_path)
--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -2,4 +2,8 @@ class UserPolicy < ApplicationPolicy
   def index?
     user.present?
   end
+
+  def password?
+    user.present?
+  end
 end
