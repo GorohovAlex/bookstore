24f7d4dbfeb4eadac7c0a8afb99b8999ae6fdc92
GorohovAlex <gorochov.as@gmail.com>
2020-02-24 14:23:04 +0200

    Add require forms

--- spec/support/pages/user_settings.rb
+++ spec/support/pages/user_settings.rb
@@ -1,5 +1,7 @@
 require_relative 'account_remove_form'
 require_relative 'email_form'
+require_relative 'address_form'
+require_relative 'password_form'
 
 class UserSettings < SitePrism::Page
   set_url '/user'
