baa81e0c97511b1b907c85acbdb60279e764aaa9
GorohovAlex <gorochov.as@gmail.com>
2020-03-02 10:43:49 +0200

    Fix bugs for id field of AddressForm

--- spec/support/pages/user_settings_page.rb
+++ spec/support/pages/user_settings_page.rb
@@ -5,9 +5,9 @@ class UserSettingsPage < SitePrism::Page
   element :privacy_tab_link, '#privacy-tab-link'
   element :notice, '#notice'
 
-  section :new_billing_address_form, AddressFormSection, 'form#new_billing_address_form'
-  section :new_shipping_address_form, AddressFormSection, 'form#new_shipping_address_form'
-  section :email_form,               EmailFormSection,         'form#new_users_email_form'
-  section :passwords_form,           PasswordsFormSection,     'form#address_form_new_password_form'
-  section :account_remove_form,      AccountRemoveFormSection, 'form#remove_account_form'
+  section :new_billing_address_form,  AddressFormSection,       'form#billing_address_form'
+  section :new_shipping_address_form, AddressFormSection,       'form#shipping_address_form'
+  section :email_form,                EmailFormSection,         'form#new_users_email_form'
+  section :passwords_form,            PasswordsFormSection,     'form#address_form_new_password_form'
+  section :account_remove_form,       AccountRemoveFormSection, 'form#remove_account_form'
 end
