8fa56fd684801b25ea3f5facd7f18883b39d594e
GorohovAlex <gorochov.as@gmail.com>
2020-03-03 23:58:18 +0200

    Create active pages

--- app/models/book.rb
+++ app/models/book.rb
@@ -23,7 +23,7 @@ class Book < ApplicationRecord
   has_many :book_authors
   has_many :authors, through: :book_authors, dependent: :destroy
 
-  has_one :book_dimension
+  has_one :book_dimension, dependent: :destroy
 
   belongs_to :category, counter_cache: true
   has_and_belongs_to_many :materials
