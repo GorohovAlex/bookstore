33489618c8211655392fd2bbf6d09cb82e0df748
GorohovAlex <gorochov.as@gmail.com>
2020-03-04 16:51:26 +0200

    Refactoring queries

--- app/presenters/address_presenter.rb
+++ app/presenters/address_presenter.rb
@@ -2,10 +2,10 @@ class AddressPresenter < Rectify::Presenter
   attribute :user_id, Integer
 
   def billing_address_form
-    User.find_by(id: user_id)&.billing_address || BillingAddress.new(user_id: user_id)
+    BillingAddress.find_or_initialize_by(user_id: user_id)
   end
 
   def shipping_address_form
-    User.find_by(id: user_id)&.shipping_address || ShippingAddress.new(user_id: user_id)
+    ShippingAddress.find_or_initialize_by(user_id: user_id)
   end
 end
