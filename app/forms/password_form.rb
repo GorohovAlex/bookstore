2fed9044fdd8aed81c4f6f63c244ee1a0133ca37
GorohovAlex <gorochov.as@gmail.com>
2020-03-04 14:32:27 +0200

    Change password form

--- app/forms/password_form.rb
+++ app/forms/password_form.rb
@@ -4,34 +4,29 @@ class PasswordForm < BaseForm
   attribute :old_password, String
   attribute :password, String
   attribute :password_confirmation, String
-  attribute :user, User
-
-  validates :old_password,
-            presence: true
+  attribute :user_id, Integer
 
   validate :verify_old_password
-
-  validates :password,
-            presence: true,
-            format: { with: User::PASSWORD_FORMAT_REGEX }
-
-  validates :password_confirmation,
+  
+  validates :old_password, presence: true
+  validates :password, :password_confirmation,
             presence: true,
             format: { with: User::PASSWORD_FORMAT_REGEX }
 
   validates_confirmation_of :password, :password_confirmation
 
   def initialize(attr = {})
+    @user = User.find_by(id: attr[:user_id])
     super
   end
 
   private
 
   def persist!
-    user.reset_password(password, password_confirmation)
+    @user.reset_password(password, password_confirmation)
   end
 
   def verify_old_password
-    errors.add(:old_password, :invalid, message: I18n.t('.not_valid')) unless user.valid_password?(old_password)
+    errors.add(:old_password, :invalid, message: I18n.t('.not_valid')) unless @user.valid_password?(old_password)
   end
 end
