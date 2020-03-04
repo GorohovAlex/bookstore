db3c0c3408f95b18023a54db1bbe4182c400d612
GorohovAlex <gorochov.as@gmail.com>
2020-03-04 16:23:43 +0200

    Change ReviewController

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,19 @@
 class ReviewsController < InheritedResources::Base
 
   def create
-    ReviewForm.new(review_params).save
+    respond_to do |format|
+      if ReviewForm.new(review_params).save
+        format.html { redirect_to user_path, flash: { notice: t('.successful_message') } }
+      else
+        format.js { render :edit, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
 
     def review_params
-      params.require(:review).permit(:title, :review, :rating)
+      params_full = params.require(:review).permit(:title, :review, :rating, :book_id)
+      params_full[:user_id] = current_user.id
     end
 end
