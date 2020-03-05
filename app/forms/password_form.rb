aa74d4c72997c9a6b434ce13806b2ca2fd0bb4e2
GorohovAlex <gorochov.as@gmail.com>
2020-03-05 19:34:49 +0200

    Change password form

--- app/forms/password_form.rb
+++ app/forms/password_form.rb
@@ -1,10 +1,10 @@
 class PasswordForm < BaseForm
   attr_accessor :password_confirmation, :password, :old_password
+  MODEL_CLASS = 'User'.freeze
 
   attribute :old_password, String
   attribute :password, String
   attribute :password_confirmation, String
-  attribute :user_id, Integer
 
   validate :verify_old_password
 
@@ -15,18 +15,13 @@ class PasswordForm < BaseForm
 
   validates_confirmation_of :password, :password_confirmation
 
-  def initialize(attr = {})
-    @user = User.find_by(id: attr[:user_id])
-    super
-  end
-
   private
 
   def persist!
-    @user.reset_password(password, password_confirmation)
+    record.reset_password(password, password_confirmation)
   end
 
   def verify_old_password
-    errors.add(:old_password, :invalid, message: I18n.t('.not_valid')) unless @user.valid_password?(old_password)
+    errors.add(:old_password, :invalid, message: I18n.t('.not_valid')) unless record.valid_password?(old_password)
   end
 end
