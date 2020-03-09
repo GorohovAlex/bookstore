82c2c1ef1bfdf44202e4d979786810ef4ad01bb0
GorohovAlex <gorochov.as@gmail.com>
2020-03-09 18:03:28 +0200

    Change reviews list

--- app/decorators/book_decorator.rb
+++ app/decorators/book_decorator.rb
@@ -20,4 +20,8 @@ class BookDecorator < ApplicationDecorator
   def description_end
     object.description[Book::DESCRIPTION_SHORT_LENGTH...object.description.size]
   end
+
+  def reviews_approved
+    object.reviews.includes(:user).filter(&:approved?)
+  end
 end
---
+++ app/decorators/user_decorator.rb
@@ -0,0 +1,8 @@
+class UserDecorator < ApplicationDecorator
+  delegate_all
+
+  def name_first_letter
+    object.name[0,1]
+  end
+
+end
