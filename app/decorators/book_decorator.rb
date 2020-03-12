bcea2c36c80188141dcaae3ece8ff537719f502e
GorohovAlex <gorochov.as@gmail.com>
2020-03-12 12:00:15 +0200

    Revert "Revert "Revert "medium"""
    This reverts commit 0af5cfc338bc0d4460c34de53e997e3b24c22a74.

--- app/decorators/book_decorator.rb
+++ app/decorators/book_decorator.rb
@@ -22,6 +22,6 @@ class BookDecorator < ApplicationDecorator
   end
 
   def main_cover_url
-    object.covers&.first&.image_url(:medium)
+    object.covers.first.image_url(:thumbnail_small)
   end
 end
