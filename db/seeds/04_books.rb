a721743c1d5e40d5024bb22342e9b7148b427e43
GorohovAlex <gorochov.as@gmail.com>
2020-03-12 10:52:35 +0200

    Move to rand

--- db/seeds/04_books.rb
+++ db/seeds/04_books.rb
@@ -3,8 +3,8 @@
   category    = Category.order('RANDOM()').first
   description = FFaker::Lorem.paragraph
   title       = FFaker::Book.title.delete(':')
-  price       = FFaker::Random.rand(20..120)
-  year        = FFaker::Random.rand(1981..2019)
+  price       = rand(20..120)
+  year        = rand(1981..2019)
   materials   = Material.order('RANDOM()').limit(rand(1..2))
   book_dimension = BookDimension.create(height: rand(5..10.0), width: rand(7..14.0), depth: rand(3..7.0))
 
--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -2,16 +2,16 @@ FactoryBot.define do
   factory :book do
     description { FFaker::Lorem.characters(Book::DESCRIPTION_SHORT_LENGTH * 2) }
     name        { FFaker::Book.title.delete(':') }
-    price       { FFaker::Random.rand(20..120) }
+    price       { rand(20..120) }
     category
-    year_of_publication { FFaker::Random.rand(1981..2019) }
+    year_of_publication { rand(1981..2019) }
 
     trait :with_authors do
-      authors { create_list :author, FFaker::Random.rand(1..2) }
+      authors { create_list :author, rand(1..2) }
     end
 
     trait :with_reviews do
-      reviews { create_list :review, FFaker::Random.rand(Review::RATING_INTERVAL) }
+      reviews { create_list :review, rand(Review::RATING_INTERVAL) }
     end
 
     after(:create) do |book|
