66438cb0864773391aa0421fe1000225d539954b
GorohovAlex <gorochov.as@gmail.com>
2020-03-15 20:39:41 +0200

    Create actions

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,15 +1,35 @@
 class CartItemsController < ApplicationController
-  def show
-    @cart_items = CartItems::AllItems.call(user_id: current_user&.id, session_id: session.id.to_s)
-    @cart_sub_total = CartItems::SubTotal.call(user_id: current_user&.id, session_id: session.id.to_s)
+  def index
+    cookies[:coupon] = params[:coupon] if params[:coupon]
+    page_values
   end
 
   def create
     cart_item_form = CartItemForm.new(cart_item_params)
     cart_item_form.save
+
     respond_to do |format|
-        # format.html { redirect_to user_path, flash: { notice: t('.successful_message', type: @address_form.type) } }
-        format.js { render partial: 'partials/header', status: :unprocessable_entity }
+      format.js { render partial: 'partials/header', status: :ok }
+    end
+  end
+
+  def update
+    quantity = params[:cart_item][:quantity].to_i.positive? ? params[:cart_item][:quantity] : 1
+    CartItem.find_by(id: params[:id]).update(quantity: quantity)
+  
+    page_values
+
+    respond_to do |format|
+      format.js { redirect_to cart_items_path, turbolink: true }
+    end
+  end
+
+  def destroy
+    CartItem.delete_by(id: params[:id])
+    page_values
+
+    respond_to do |format|
+      format.js { redirect_to cart_items_path, turbolink: true }
     end
   end
 
@@ -17,5 +37,13 @@ class CartItemsController < ApplicationController
 
   def cart_item_params
     params.require(:cart_item).permit(:book_id, :quantity).merge(session_id: session.id.to_s, user_id: current_user&.id)
+  end
+
+  def page_values
+    @cart_items = CartItems::AllItems.call(user_id: current_user&.id, session_id: session.id.to_s)
+    @cart_sub_total = CartItems::SubTotal.call(user_id: current_user&.id, session_id: session.id.to_s)
+    @coupon = cookies[:coupon]
+    @discount = Coupons::Discount.call(coupon: @coupon).to_money
+    @order_total = @cart_sub_total + @discount
   end
 end
