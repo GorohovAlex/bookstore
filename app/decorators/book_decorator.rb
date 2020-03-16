c3a3bf91078256effd313cad0055f191a4f78f71
GorohovAlex <gorochov.as@gmail.com>
2020-03-16 10:02:04 +0200

    Add default image

--- app/decorators/book_decorator.rb
+++ app/decorators/book_decorator.rb
@@ -1,6 +1,8 @@
 class BookDecorator < ApplicationDecorator
   delegate_all
 
+  DEFAULT_COVER_URL = 'https://www.bramstein.com/static/smashing-book-5-photo-stack.9d5d114e3e.jpg'
+
   def authors_show
     object.authors.collect { |author| "#{author.last_name} #{author.first_name}" }.join(', ')
   end
@@ -22,7 +24,7 @@ class BookDecorator < ApplicationDecorator
   end
 
   def main_cover_url
-    object.covers&.first&.image_url(:large)
+    object.covers&.first&.image_url(:large) || DEFAULT_COVER_URL
   end
 
   def secondary_covers
