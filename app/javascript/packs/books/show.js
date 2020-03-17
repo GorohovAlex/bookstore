223e0ec68add5ce999abe5228be2aeade4ea29a8
GorohovAlex <gorochov.as@gmail.com>
2020-03-17 15:07:49 +0200

    Change counter js

--- app/javascript/packs/books/show.js
+++ app/javascript/packs/books/show.js
@@ -1,17 +1,19 @@
 $(document).ready(function() {
-  $('#quantity-plus').on('click', function(){
+  $('a.quantity-plus').on('click', function(){
     event.preventDefault()
-    var value = $("#quantity-value").val()
-    value = isNaN(value) || value == '' ? BOOK_ORDER_COUNT_DEFAULT : parseInt(value) + 1
-    $('#quantity-value').val(value)
+    change_quantity(this.parentElement, 1)
   });
-
-  $("#quantity-minus").on('click', function(){
+  
+  $("a.quantity-minus").on('click', function(){
     event.preventDefault()
-    var value = $("#quantity-value").val()
-    value = isNaN(value) || value == '' || value == '1' ? 1 : parseInt(value) - 1
-    $("#quantity-value").val(value)
+    change_quantity(this.parentElement, -1)
   });
+  
+  function change_quantity(form, value){
+    var quantity_input = form.getElementsByClassName('quantity-input')[0]
+    value = parseInt(quantity_input.value) + value
+    quantity_input.value = isNaN(value) || value == '' || value < 1 ? 1 : value
+  }
 
   $("#read_more").on('click', function(){
     event.preventDefault()
@@ -24,37 +26,3 @@ $(document).ready(function() {
     btnMore.style.display = "none";
   });
 });
-
-
-/*
-
-$(document).ready(function() {
-  $('.quantity-plus').on('click', function(){
-    event.preventDefault()
-    var group = $(this).parent()
-    var input = group[0].querySelector('input')
-    // var value = input.val()
-    console.log(input)
-    // value = isNaN(value) || value == '' ? BOOK_ORDER_COUNT_DEFAULT : parseInt(value) + 1
-    // $('#quantity-value').val(value)
-  });
-
-  $(".quantity-minus").on('click', function(){
-    event.preventDefault()
-  //   var value = $("#quantity-value").val()
-  //   value = isNaN(value) || value == '' || value == '1' ? 1 : parseInt(value) - 1
-  //   $("#quantity-value").val(value)
-  });
-
-  $("#read_more").on('click', function(){
-    event.preventDefault()
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnMore = document.getElementById("read_more");
-   
-    dots.style.display = "none";
-    moreText.style.display = "inline";
-    btnMore.style.display = "none";
-  });
-});
-*/
--- app/javascript/packs/cart_items/index.js
+++ app/javascript/packs/cart_items/index.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
   $(".quantity-plus").on('click', function(){
     event.preventDefault();
-    var form = this.parentElement
-    var quantity_input = form.getElementsByClassName('quantity-input')[0]
-    quantity_input.value = parseInt(quantity_input.value) + 1
-    form.dispatchEvent(new Event('submit', {bubbles: true}));
+    change_quantity(this.parentElement, 1)
   });
-
+  
   $("a.quantity-minus").on('click', function(){
     event.preventDefault();
-    var form = this.parentElement
-    var quantity_input = form.getElementsByClassName('quantity-input')[0]
-    quantity_input.value = parseInt(quantity_input.value) - 1
-    form.dispatchEvent(new Event('submit', {bubbles: true}));
+    change_quantity(this.parentElement, -1)
   });
+  
+  function change_quantity(form, value){
+    var quantity_input = form.getElementsByClassName('quantity-input')[0]
+    quantity_input.value = parseInt(quantity_input.value) + value
+    form.dispatchEvent(new Event('submit', {bubbles: true}));
+  }
 });
