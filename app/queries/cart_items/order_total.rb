e8668ecbf20930fdc437a965b4daf9619b2cac3d
GorohovAlex <gorochov.as@gmail.com>
2020-03-23 15:35:59 +0200

    Create OrderTotal queries

---
+++ app/queries/cart_items/order_total.rb
@@ -0,0 +1,9 @@
+module CartItems
+  class OrderTotal < AllItems
+    def self.call(user_id: nil, session_id: nil, coupon: nil)
+      discount = Coupons::Discount.call(coupon: coupon).to_money
+      cart_sub_total = CartItems::SubTotal.call(user_id: user_id, session_id: session_id)
+      discount.abs < cart_sub_total ? cart_sub_total + discount : 0
+    end
+  end
+end
