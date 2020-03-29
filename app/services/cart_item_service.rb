5f357d8e57749f7af7690a94091d9a263a62d20b
GorohovAlex <gorochov.as@gmail.com>
2020-03-29 21:58:12 +0300

    Create CartItemService

--- app/services/cart_item_service.rb
+++ app/services/cart_item_service.rb
@@ -1,7 +1,20 @@
 class CartItemService
-  def initialize(user_id: nil, session_id: nil)
+  def initialize(user_id: nil, session_id: nil, coupon: nil)
     @user_id = user_id
     @session_id = session_id
+    @coupon = coupon
+  end
+
+  def cart_sub_total
+    @cart_sub_total ||= CartItems::SubTotal.call(user_id: @current_user_id, session_id: @session_id)
+  end
+
+  def discount
+    @discount ||= Coupons::Discount.call(coupon: @coupon).to_money
+  end
+
+  def order_total
+    @order_total = discount.abs < cart_sub_total ? cart_sub_total + discount : 0
   end
 
   def join_cart_items
