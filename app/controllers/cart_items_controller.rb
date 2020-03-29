cfd66e3dcbddf5a6fc5f2ade606d3798f55f43e4
GorohovAlex <gorochov.as@gmail.com>
2020-03-29 21:58:24 +0300

    Change CartItemsController

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,22 +1,22 @@
 class CartItemsController < ApplicationController
+  before_action :authorize_resource
+
   def index
     cookies[:coupon] = params[:coupon] if params[:coupon]
-    page_values
+    @cart_items = policy_scope(CartItem)
+    cart_total_values
   end
 
   def create
     cart_item_form = CartItemForm.new(cart_item_params)
     cart_item_form.save
-
     respond_to do |format|
       format.js { render partial: 'partials/header', status: :ok }
     end
   end
 
   def update
-    authorize_resource
     @cart_item.update(quantity: quantity)
-    page_values
 
     respond_to do |format|
       format.js { redirect_to cart_items_path, turbolink: true }
@@ -24,9 +24,7 @@ class CartItemsController < ApplicationController
   end
 
   def destroy
-    authorize_resource
     CartItem.delete(@cart_item)
-    page_values
 
     respond_to do |format|
       format.js { redirect_to cart_items_path, turbolink: true }
@@ -36,9 +34,13 @@ class CartItemsController < ApplicationController
   private
 
   def authorize_resource
-    @cart_item = CartItem.where(CartItems::CartBase.cart_items_params(current_user, session.id.to_s)
-                                                   .merge(id: params[:id]))
+    @cart_item = policy_scope(CartItem)
+    @cart_item = @cart_item.where(id: params[:id]) unless params[:id].nil?
     authorize @cart_item
+  end
+
+  def pundit_user
+    { user: current_user, session_id: session.id.to_s }
   end
 
   def quantity
@@ -46,33 +48,16 @@ class CartItemsController < ApplicationController
   end
 
   def cart_item_params
-    params.require(:cart_item).permit(:book_id, :quantity).merge(session_id: session.id.to_s, user_id: current_user&.id)
-  end
-
-  def page_values
-    @cart_items = CartItems::AllItems.call(user_id: current_user&.id, session_id: session.id.to_s)
-    cart_total_values
+    params.require(:cart_item).permit(:book_id, :quantity)
+          .merge(session_id: session.id.to_s, user_id: current_user&.id)
   end
 
   def cart_total_values
-    discount
-    cart_sub_total
-    order_total
-  end
+    cart_item_service = CartItemService.new(user_id: current_user&.id, session_id: session.id.to_s,
+                                            coupon: cookies[:coupon])
 
-  def discount
-    @discount = Coupons::Discount.call(coupon: coupon).to_money
-  end
-
-  def cart_sub_total
-    @cart_sub_total = CartItems::SubTotal.call(user_id: current_user&.id, session_id: session.id.to_s)
-  end
-
-  def order_total
-    @order_total = CartItems::OrderTotal.call(user_id: current_user&.id, session_id: session.id.to_s, coupon: coupon)
-  end
-
-  def coupon
-    @coupon ||= cookies[:coupon]
+    @cart_sub_total = cart_item_service.cart_sub_total
+    @discount = cart_item_service.discount.to_money
+    @order_total = cart_item_service.order_total
   end
 end
