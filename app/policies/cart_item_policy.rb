e9f8e28a3723ac1197024cf4feefcfb575e24684
GorohovAlex <gorochov.as@gmail.com>
2020-03-29 21:56:36 +0300

    Change CartItemPolicy

--- app/policies/cart_item_policy.rb
+++ app/policies/cart_item_policy.rb
@@ -1,9 +1,25 @@
 class CartItemPolicy < ApplicationPolicy
+  def index?
+    true
+  end
+
+  def create?
+    true
+  end
+
   def update?
     record.any?
   end
 
   def destroy?
-    record.any?
+    update?
+  end
+
+  class Scope < Scope
+    attr_reader :user, :scope
+
+    def resolve
+      scope.where(user[:user].nil? ? { session_id: user[:session_id] } : { user_id: user[:user].id })
+    end
   end
 end
