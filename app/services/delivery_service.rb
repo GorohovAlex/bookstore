545de9b2575cf6671ff45161c9753422ea555e25
GorohovAlex <gorochov.as@gmail.com>
2020-04-01 01:14:35 +0300

    Create DeliveryService

---
+++ app/services/delivery_service.rb
@@ -0,0 +1,18 @@
+class DeliveryService
+  def initialize(current_order:, params: [])
+    @current_order = current_order
+    @params = params
+  end
+
+  def call
+    delivery = Delivery.find_by!(id: @params[:order][:order_delivery].to_i)
+    order_delivery = OrderDelivery.find_or_initialize_by(order: @current_order)
+    order_delivery.delivery_id = delivery.id
+
+    @current_order.update(order_delivery: order_delivery)
+  end
+
+  def presenter
+    
+  end
+end
