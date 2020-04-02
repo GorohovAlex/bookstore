86cbefe015b0f33bba3a35617bf20304ed2f41ea
GorohovAlex <gorochov.as@gmail.com>
2020-04-03 00:40:48 +0300

    Add validates

--- app/forms/card_form.rb
+++ app/forms/card_form.rb
@@ -4,6 +4,8 @@ class CardForm < BaseForm
   MODEL_CLASS = 'Card'.freeze
 
   DATE_EXPIRY_REGEX = %r{\A(0[1-9]|10|11|12)/[0-9]{2}\z}.freeze
+  NAME_REGEX = /\A[a-zA-Z\s]+\z/.freeze
+  CVV_REGEX = /\A[0-9]+\z/.freeze
 
   attribute :order_id, Integer
   attribute :number, String
@@ -12,9 +14,9 @@ class CardForm < BaseForm
   attribute :cvv, String
 
   validates :number, presence: true, credit_card_number: true
-  validates :name, presence: true
-  validates :date_expiry, presence: true, format: { with: DATE_EXPIRY_REGEX }
-  validates :cvv, presence: true
+  validates :name, presence: true, format: { with: NAME_REGEX, message: 'only allows letters and space' }
+  validates :date_expiry, presence: true, format: { with: DATE_EXPIRY_REGEX, message: 'example: 12/22' }
+  validates :cvv, presence: true, format: { with: CVV_REGEX, message: 'only allows numbers' }
   validates :cvv, length: { is: 3 }, unless: :card_american_express?
   validates :cvv, length: { is: 4 }, if: :card_american_express?
 
