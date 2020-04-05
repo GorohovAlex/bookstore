4307f95cb39134a4e0a284cef23a2bfed10f652d
GorohovAlex <gorochov.as@gmail.com>
2020-04-06 00:11:06 +0300

    Add cascade on delete and change type

--- db/migrate/20200401131210_create_cards.rb
+++ db/migrate/20200401131210_create_cards.rb
@@ -3,9 +3,9 @@ class CreateCards < ActiveRecord::Migration[6.0]
     create_table :cards do |t|
       t.string :number
       t.string :name
-      t.date :date_expiry
+      t.string :date_expiry
       t.string :cvv
-      t.belongs_to :order, null: false, foreign_key: true
+      t.belongs_to :order, null: false, foreign_key: { on_delete: :cascade }
 
       t.timestamps
     end
