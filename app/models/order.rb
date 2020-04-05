dfa8334b9d1ee9e865ce645b1f3532d4191b618f
GorohovAlex <gorochov.as@gmail.com>
2020-04-06 00:06:38 +0300

    Add events

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,6 +7,7 @@ class Order < ApplicationRecord
   has_one  :billing_address, as: :owner, dependent: :destroy
   has_one  :order_delivery, dependent: :destroy
   has_one  :card, dependent: :destroy
+  has_many :order_items, dependent: :destroy
 
   aasm do
     state :address, initial: true
@@ -15,11 +16,40 @@ class Order < ApplicationRecord
     state :confirm
     state :complete
 
-    event :address do
+    event :to_address do
+      transitions from: :confirm, to: :address
+    end
+
+    event :to_delivery do
+      transitions from: :confirm, to: :delivery
+      transitions from: :address, to: :confirm, guard: :states_completed?
       transitions from: :address, to: :delivery
     end
-    event :delivery do
+
+    event :to_payment do
+      transitions from: :confirm, to: :payment
+      transitions from: :delivery, to: :confirm, guard: :card_completed?
       transitions from: :delivery, to: :payment
     end
+
+    event :to_confirm do
+      transitions from: :payment, to: :confirm
+    end
+
+    event :to_complete do
+      transitions from: :confirm, to: :complete
+    end
+  end
+
+  def states_completed?
+    delivery_completed? && card_completed?
+  end
+
+  def delivery_completed?
+    order_delivery.present?
+  end
+
+  def card_completed?
+    card.present?
   end
 end
