7af25317f09bb10f16d32948462d4e76376302e8
GorohovAlex <gorochov.as@gmail.com>
2020-04-14 22:54:07 +0300

    Fix shipping_address

--- app/presenters/checkouts/confirm_presenter.rb
+++ app/presenters/checkouts/confirm_presenter.rb
@@ -7,7 +7,7 @@ module Checkouts
     def shipping_address_info_items
       return owner.billing_address.decorate.address_info_items if owner.use_billing_address
 
-      owner.billingshipping_address.decorate.address_info_items
+      owner.shipping_address.decorate.address_info_items
     end
 
     def shipments_info_items
