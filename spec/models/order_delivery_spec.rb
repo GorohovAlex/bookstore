e5d1d423149d2cd3d7fe90d6d50727bac5cfd8e4
GorohovAlex <gorochov.as@gmail.com>
2020-04-15 12:43:49 +0300

    Change order models specs

--- spec/models/order_delivery_spec.rb
+++ spec/models/order_delivery_spec.rb
@@ -1,5 +1,4 @@
-require 'rails_helper'
-
 RSpec.describe OrderDelivery, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to(:order) }
+  it { is_expected.to belong_to(:delivery) }
 end
