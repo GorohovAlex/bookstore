d557f7af587b9ae3387374166560e0fd3b3ff1e4
GorohovAlex <gorochov.as@gmail.com>
2020-04-21 11:05:31 +0300

    require section

--- spec/support/pages/sections/card_form_section.rb
+++ spec/support/pages/sections/card_form_section.rb
@@ -1,3 +1,5 @@
+require_relative 'form_group_input_section'
+
 class CardFormSection < SitePrism::Section
   section :card_number, FormGroupInputSection, '.number-group'
   section :card_name,   FormGroupInputSection, '.name-group'
