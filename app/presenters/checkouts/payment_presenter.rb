2bf82a04694abf598a586e8e53ad16bf9038063e
GorohovAlex <gorochov.as@gmail.com>
2020-04-25 22:09:50 +0300

    Remove initialize

--- app/presenters/checkouts/payment_presenter.rb
+++ app/presenters/checkouts/payment_presenter.rb
@@ -1,14 +1,8 @@
 module Checkouts
   class PaymentPresenter < CheckoutBasePresenter
     attribute :order_card_form, OrderCardForm
-
-    def initialize(owner: nil, coupon_name: nil, summary_items: nil, order_card_form: nil)
-      super(owner: owner, coupon_name: coupon_name, summary_items: summary_items)
-      @order_card_form = order_card_form
-    end
-
     def order_card_form
-      @order_card_form || OrderCard.find_or_initialize_by(order: owner)
+      @order_card_form || OrderCardForm.new
     end
   end
 end
