51c196ea2f9f7161a0873a07378dee46a70f7cf3
GorohovAlex <gorochov.as@gmail.com>
2020-04-25 22:04:33 +0300

    Separate and memoization

--- app/presenters/checkouts/address_presenter.rb
+++ app/presenters/checkouts/address_presenter.rb
@@ -3,20 +3,22 @@ module Checkouts
     attribute :billing_address_form, BillingAddressForm
     attribute :shipping_address_form, ShippingAddressForm
 
-    def initialize(owner: nil, billing_address_form: nil, shipping_address_form: nil, coupon_name: nil)
-      @billing_address_form = billing_address_form
-      @shipping_address_form = shipping_address_form
-      super(owner: owner, coupon_name: coupon_name)
-    end
-
     def billing_address_form
-      params = (owner.is_a? Order) && owner.billing_address.blank? ? owner.user : owner
-      @billing_address_form || BillingAddress.find_or_initialize_by(owner: params)
+      @billing_address_form || BillingAddress.find_or_initialize_by(owner: billing_address_params)
     end
 
     def shipping_address_form
-      params = (owner.is_a? Order) && owner.shipping_address.blank? ? owner.user : owner
-      @shipping_address_form || ShippingAddress.find_or_initialize_by(owner: params)
+      @shipping_address_form || ShippingAddress.find_or_initialize_by(owner: shipping_address_params)
+    end
+
+    private
+
+    def billing_address_params
+      (owner.is_a? Order) && owner.billing_address.blank? ? owner.user : owner
+    end
+
+    def shipping_address_params
+      (owner.is_a? Order) && owner.shipping_address.blank? ? owner.user : owner
     end
   end
 end
