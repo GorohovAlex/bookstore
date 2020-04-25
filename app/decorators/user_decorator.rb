ff170da4d7165df44a39a728ac73bab29fd33cfc
GorohovAlex <gorochov.as@gmail.com>
2020-04-25 21:59:12 +0300

    Move method to User  model

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,13 +1,7 @@
 class UserDecorator < ApplicationDecorator
   delegate_all
 
-  CHECKOUT_STATUSES = %w[address delivery payment confirm complete].freeze
-
   def name_first_letter
     object.name.first
-  end
-
-  def not_finish_orders
-    object.orders.where(aasm_state: CHECKOUT_STATUSES)
   end
 end
