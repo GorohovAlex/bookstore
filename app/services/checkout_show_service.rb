503bed3a7abc9beefe061dc7e5482a83bd917d3b
GorohovAlex <gorochov.as@gmail.com>
2020-04-25 22:12:38 +0300

    Rename method

--- app/services/checkout_show_service.rb
+++ app/services/checkout_show_service.rb
@@ -2,12 +2,12 @@ class CheckoutShowService < CheckoutBaseService
   PRESENTER_SUFIX = 'Presenter'.freeze
   PRESENTER_PREFIX = 'Checkouts::'.freeze
 
-  def call
+  def current_state
     current_order.aasm_state
   end
 
   def presenter
-    presenter_name.constantize.new(owner: current_order, coupon_name: @coupon&.name)
+    presenter_name.constantize.new(owner: current_order, coupon_name: @coupon&.name, user_id: @current_user.id)
   end
 
   private
