b345fd719b78d0cfa71120309fbf5c33a1a4b104
GorohovAlex <gorochov.as@gmail.com>
2020-04-27 18:14:25 +0300

    Create user orders page

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
     patch 'user/password', to: 'devise/user#update', as: 'update_user_password'
     patch 'user/email',    to: 'devise/user#update', as: 'update_user_email'
     patch 'user/address',  to: 'address#create',     as: 'user_address_forms'
+    get   'user/orders',   to: 'orders#index',       as: 'user_orders'
   end
 
   get 'catalog', to: 'page#catalog'
