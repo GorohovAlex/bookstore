8ce3275bc3689fae69e4e5d9a54215d547241085
GorohovAlex <gorochov.as@gmail.com>
2020-05-05 12:46:59 +0300

    Change foreign key add cascade on delete

--- db/migrate/20200324134343_create_orders.rb
+++ db/migrate/20200324134343_create_orders.rb
@@ -1,7 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: { on_delete: :cascade }
       t.string :aasm_state
       t.string :number
       t.boolean :use_billing_address
