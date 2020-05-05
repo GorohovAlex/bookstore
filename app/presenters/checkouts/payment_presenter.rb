1959eeef0b2f9e2ef4a20fa6b55527b1159af482
GorohovAlex <gorochov.as@gmail.com>
2020-05-05 13:36:32 +0300

    Fix payment presenter order_card_form

--- app/presenters/checkouts/payment_presenter.rb
+++ app/presenters/checkouts/payment_presenter.rb
@@ -3,7 +3,7 @@ module Checkouts
     attribute :order_card_form, OrderCardForm
 
     def order_card_form
-      @order_card_form ||= OrderCardForm.new
+      @order_card_form ||= OrderCard.find_or_initialize_by(order: owner)
     end
   end
 end
